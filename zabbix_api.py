ef57848939390f425666e42fe4549b0b8a9901f3
mcgyani <maican.catalin@yahoo.com>
2015-05-20 09:21:29 +0300

    Create zabbix_api.py

---
+++ zabbix_api.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from zabbix_api import ZabbixAPI
+import json
+
+zapi = ZabbixAPI(server="http://localhost/zabbix")
+zapi.login("Admin", "zabbix")
+
+hostgroup = "Zabbix servers"
+
+hostgroup = zapi.host.get({"output": "extend"
+	}
+)
+
+for host in hostgroup:
+	for key, value in host.iteritems():
+		if key == "error":
+			print value
+
+trigger_on = zapi.trigger.get({
+	"output": [
+		"triggerid",
+		"description",
+		"priority"
+	],
+	"filter": {
+		"value": 1
+	}
+})
+for trigeron in trigger_on:
+	print trigeron["description"]
+	
+#print hostgroup
+print json.dumps(trigger_on, indent = 4) 
