6220fbc62c118d587a49580ffe01d6b58bc42c38
mcgyani <ginelmaican@gmail.com>
2015-06-14 13:28:39 +0300

    Update zabbix_api.py

--- zabbix_api.py
+++ zabbix_api.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 
-from zabbix_api import ZabbixAPI
+from zabbix_api import ZabbixAPI, ZabbixAPIException
 import json
 
 zapi = ZabbixAPI(server="http://localhost/zabbix")
 zapi.login("Admin", "zabbix")
 
-hostgroup = "Zabbix servers"
 
-hostgroup = zapi.host.get({"output": "extend"
+hosts = zapi.host.get({"output": "extend", "monitored_hosts": 1
 	}
 )
 
-for host in hostgroup:
+for host in hosts:
 	for key, value in host.iteritems():
 		if key == "error":
 			print value
@@ -25,9 +24,21 @@ trigger_on = zapi.trigger.get({
 	],
 	"filter": {
 		"value": 1
-	}
+	},
+	"monitored": True,
+	"active": True
 })
 for trigeron in trigger_on:
 	print trigeron["description"]
 	
+trigger_on_ex = zapi.trigger.get({
+	"output": "extend",
+	"filter": {
+		"value": 1
+	},
+	"active": True
+})
+
+print hosts
 print json.dumps(trigger_on, indent = 4) 
+print trigger_on_ex
